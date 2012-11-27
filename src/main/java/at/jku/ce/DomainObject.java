8b050881377c9b271646c2bcc244bd3ba640efee
Artur <patrick.pils@gmail.com>
2012-11-27 12:23:11 +0100

    <Tag 4> Artur: Comment mit Getter und Setter eingefügt

--- src/main/java/at/jku/ce/DomainObject.java
+++ src/main/java/at/jku/ce/DomainObject.java
@@ -4,8 +4,9 @@ import java.util.UUID;
 
 public class DomainObject {
 
-	private String uuid= UUID.randomUUID().toString();
+	private String uuid = UUID.randomUUID().toString();
 	private String name;
+	private String comment;
 
 	/**
 	 * Default constructor
@@ -21,5 +22,13 @@ public class DomainObject {
 
 	public String getUuid() {
 		return uuid;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
 	}
 }
