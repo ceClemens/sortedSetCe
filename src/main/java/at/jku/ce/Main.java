f358e7cfeeebc146ed54aa4299b2bed2d57d6d50
Clemens <ka.regensburger@gmx.at>
2012-11-27 11:17:05 +0100

    <Tag 1> Clemens: "Loggerausgabe hinzugefügt"

--- src/main/java/at/jku/ce/Main.java
+++ src/main/java/at/jku/ce/Main.java
@@ -1,11 +1,25 @@
 package main.java.at.jku.ce;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Set<DomainObject> objectSet = new HashSet<DomainObject>();
+		objectSet.add(new DomainObject("Helga"));
+		objectSet.add(new DomainObject("Walter"));
+		objectSet.add(new DomainObject("Hans"));
+		objectSet.add(new DomainObject("Paul"));
+		objectSet.add(new DomainObject("Claudia"));
+		for (DomainObject domainObject : objectSet) {
+			LOGGER.debug(domainObject.id);
+		}
 	}
 
 }
